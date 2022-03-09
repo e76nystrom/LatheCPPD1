406d8eea7de37e241d67cc41470f3c9fbd9dab75
Eric Nystrom <eric.nystrom@yahoo.com>
2022-03-08 19:21:32 -0500

    New cube version.  Change UART5 to 19200.

--- Inc/main.h
+++ Inc/main.h
@@ -237,5 +237,3 @@ void Error_Handler(void);
 #endif
 
 #endif /* __MAIN_H */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
--- Inc/stm32f4xx_it.h
+++ Inc/stm32f4xx_it.h
@@ -78,5 +78,3 @@ void SysTick_Handler(void);
 #endif
 
 #endif /* __STM32F4xx_IT_H */
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
